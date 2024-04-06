ea9e9ef2cd23d557e0a31ba68c62f58952584a9a
agqaauto <aurum408@gmail.com>
2024-04-06 22:52:25 +0500

    Seconf Task is done

--- DZ1/src/MainClassTest.java
+++ DZ1/src/MainClassTest.java
@@ -7,7 +7,14 @@ public class MainClassTest {
     public void testGetLocalNumber()
     {
         MainClass mainClass = new MainClass();
-        Assert.assertEquals("Values are not equals " ,14, mainClass.getLocalNumber());
+        Assert.assertEquals("Values are not equals." ,14, mainClass.getLocalNumber());
+    }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        MainClass mainClass1 = new MainClass();
+        Assert.assertTrue("Value is not greater that 45.", mainClass1.getClassNumber() > 45);
     }
 
 }
