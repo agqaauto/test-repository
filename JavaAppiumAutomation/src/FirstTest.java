0707e54df1fdfa426c1ab1f92ff6e3ca3f09eafe
agqaauto <aurum408@gmail.com>
2024-05-02 22:51:03 +0500

    Ex5: Тест: сохранение двух статей

--- JavaAppiumAutomation/src/FirstTest.java
+++ JavaAppiumAutomation/src/FirstTest.java
@@ -1,10 +1,13 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -28,7 +31,8 @@ public class FirstTest {
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "C:/Users/a.nurmukhamed/Documents/GitHub/test-repository/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
-        driver = new AndroidDriver(new URL("http://192.168.100.30:4723/wd/hub"), capabilities);
+        //driver = new AndroidDriver(new URL("http://192.168.100.30:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL("http://192.168.1.101:4723/wd/hub"), capabilities);
     }
 
     @After
@@ -66,8 +70,7 @@ public class FirstTest {
     }
 
     @Test
-    public void testCancelSearch()
-    {
+    public void testCancelSearch() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
                 "Cannot find Search Wikipedia input",
@@ -87,7 +90,7 @@ public class FirstTest {
                 15
         );
 
-        List<WebElement> resultElements = results.findElements(By.id("org.wikipedia:id/page_list_item_container")) ;
+        List<WebElement> resultElements = results.findElements(By.id("org.wikipedia:id/page_list_item_container"));
 
         Assert.assertNotNull(resultElements);
         Assert.assertTrue("Elements less than 2", resultElements.size() > 1);
@@ -98,7 +101,7 @@ public class FirstTest {
                 5
         );
 
-         boolean resultsNotPresent = waitForElementNotPresent(
+        boolean resultsNotPresent = waitForElementNotPresent(
                 By.id("org.wikipedia:id/search_results_list"),
                 "Result are not present on the page",
                 15
@@ -108,8 +111,7 @@ public class FirstTest {
     }
 
     @Test
-    public void testCheckWordsInSearch()
-    {
+    public void testCheckWordsInSearch() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
                 "Cannot find Search Wikipedia input",
@@ -131,10 +133,132 @@ public class FirstTest {
 
         List<WebElement> resultElements = results.findElements(By.id("org.wikipedia:id/page_list_item_title"));
 
-        for(WebElement element : resultElements) {
+        for (WebElement element : resultElements) {
             String element_text = element.getAttribute("text");
-            Assert.assertTrue("Not contains expected text",element_text.toUpperCase().contains("JAVA"));
+            Assert.assertTrue("Not contains expected text", element_text.toUpperCase().contains("JAVA"));
         }
+    }
+
+    @Test
+    public void savingTwoArticles() {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                "Test is failed because onboarding page is not skipped",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia input",
+                5
+        );
+
+        String search_line = "Java";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                search_line,
+                "Cannot find Search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_description'][@text='Object-oriented programming language']"),
+                "Cannot find needed article in search results",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/page_save"),
+                "Cannot click to save button from menu 1",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.Button[@text='Add to list']"),
+                "Cannot click to save button from menu 2",
+                5
+        );
+
+        String name_of_folder = "Learning Programming";
+        waitForElementAndSendKeys(
+                By.xpath("//*[@text='Name of this list']"),
+                name_of_folder,
+                "Cannot enter name of Test",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = 'OK']"),
+                "Cannot press OK",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot navigate back on the Article",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.TextView[@text='Java']"),
+                "Cannot find needed article in search results",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/page_save"),
+                "Cannot click to save button from menu 3",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.Button[@text='Add to list']"),
+                "Cannot click to save button from menu 4",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.TextView[@text='" + name_of_folder + "']"),
+                "Cannot click to save button from menu 5",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.Button[@text='View list']"),
+                "Cannot click to save button from menu 6",
+                5
+        );
+
+        swipeElementToLeft(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Caanot find saved article"
+        );
+
+        waitForElementNotPresent(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot delete saved article",
+                5
+        );
+
+        String result_of_articles = "org.wikipedia:id/page_list_item_container";
+        int element_count = getAmountOfElements(By.id(result_of_articles));
+
+        Assert.assertTrue("Result of articles is not equals 1", element_count == 1);
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = '" + search_line + "']"),
+                "Cannot open the second article",
+                15
+        );
+
+        String title_article = waitForElementAndGetAttribute(
+                By.xpath("//*[@text='" + search_line + "']"),
+                "text",
+                "Title of article is not present",
+                15
+        );
+
+        Assert.assertEquals("Articles not the same", search_line, title_article);
     }
 
     private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds) {
@@ -170,5 +294,35 @@ public class FirstTest {
         return wait.until(
                 ExpectedConditions.invisibilityOfElementLocated(by)
         );
+    }
+
+    protected void swipeElementToLeft(By by, String error_message) {
+        WebElement element = waitForElementPresent(
+                by,
+                error_message,
+                10);
+        int left_x = element.getLocation().getX();
+        int right_x = left_x + element.getSize().getWidth();
+        int upper_y = element.getLocation().getY();
+        int lower_y = upper_y + element.getSize().getHeight();
+        int middle_y = (upper_y + lower_y) / 2;
+
+        TouchAction action = new TouchAction(driver);
+        action
+                .press(right_x, middle_y)
+                .waitAction(300)
+                .moveTo(left_x, middle_y)
+                .release()
+                .perform();
+    }
+
+    private int getAmountOfElements(By by) {
+        List elements = driver.findElements(by);
+        return elements.size();
+    }
+
+    private String waitForElementAndGetAttribute(By by, String attribute, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        return element.getAttribute(attribute);
     }
 }
